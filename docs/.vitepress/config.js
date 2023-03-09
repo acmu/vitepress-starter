12c2e8ed6e830cf2c76463923d5784008395cd1a
zhengmingyuan02 <zhengmingyuan02@meituan.com>
2023-03-09 13:36:59 +0800

    fix: end

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -26,6 +26,8 @@ export default defineConfig({
     //     ],
     // ],
 
+    head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }]],
+
     // 1
     // 2
     // 3
@@ -56,11 +58,17 @@ export default defineConfig({
                 text: '知识',
                 items: [
                     { text: '浮点数与二进制', link: '/knowledge/01' },
-                    { text: 'ESLint 和 Prettier 规范代码', link: '/knowledge/02' },
+                    {
+                        text: 'ESLint 和 Prettier 规范代码',
+                        link: '/knowledge/02',
+                    },
                     { text: '力扣 504 七进制转换', link: '/knowledge/03' },
                     { text: 'NPM 发布 TS 包', link: '/knowledge/04' },
                     { text: '小鹤双拼推荐', link: '/knowledge/05' },
-                    { text: '在 Mac 中像 Windows 一样切换应用', link: '/knowledge/06' },
+                    {
+                        text: '在 Mac 中像 Windows 一样切换应用',
+                        link: '/knowledge/06',
+                    },
                     { text: 'Chrome 96 更新', link: '/knowledge/07' },
                     { text: '浏览器插件入门实践', link: '/knowledge/08' },
                 ],
@@ -68,12 +76,24 @@ export default defineConfig({
             {
                 text: 'JavaScript',
                 items: [
-                    { text: 'defineProperty 详细解读', link: '/JavaScript/101' },
+                    {
+                        text: 'defineProperty 详细解读',
+                        link: '/JavaScript/101',
+                    },
                     { text: 'JavaScript 对象解读', link: '/JavaScript/102' },
-                    { text: 'JavaScript 的对象与继承', link: '/JavaScript/103' },
-                    { text: 'JavaScript 异步与 Promise', link: '/JavaScript/104' },
+                    {
+                        text: 'JavaScript 的对象与继承',
+                        link: '/JavaScript/103',
+                    },
+                    {
+                        text: 'JavaScript 异步与 Promise',
+                        link: '/JavaScript/104',
+                    },
                     { text: 'JavaScript 正则理解', link: '/JavaScript/105' },
-                    { text: '周下载量超1亿的库是如何判断JS类型的？', link: '/JavaScript/106' },
+                    {
+                        text: '周下载量超1亿的库是如何判断JS类型的？',
+                        link: '/JavaScript/106',
+                    },
                     { text: 'JavaScript Symbol 解读', link: '/JavaScript/107' },
                     { text: '使用 npm link 调试包', link: '/JavaScript/108' },
                     { text: '可选链运算符', link: '/JavaScript/109' },
